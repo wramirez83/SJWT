7f583b2c05e68b59392ed300a7289e5c71daf220
Wilson Ramirez Z <wilson.rz@gmail.com>
2022-06-06 14:27:18 -0500

    decode

--- src/SJWT.php
+++ src/SJWT.php
@@ -3,27 +3,58 @@
 namespace Wramirez83\Sjwt;
 use Wramirez83\Sjwt\Tools\StructJWT;
 use Wramirez83\Sjwt\Tools\UrlEncode;
-require ('./../bootstrap.php');
+use Carbon\Carbon;
 
 class SJWT{
 
-    protected $header;
-    protected $payload;
-    protected $secret;
+    protected $tokenExpired;
+    protected $signatureValid;
 
     public function __construct(){
        
         
     }
 
-    public function encode($payload, $exp = 60){
-        $this->secret = getenv('SECRET_JWT');
-        $this->header = StructJWT::setHeader();
-        $this->payload = StructJWT::setPayload($payload, $exp);
-        $signature = hash_hmac('sha256', $this->header . '.' . $this->payload, $this->secret, true);
+    public static function encode($payload, $exp = 60){
+        $secret = getenv('SECRET_JWT');
+        $header = StructJWT::setHeader();
+        $payload = StructJWT::setPayload($payload, $exp);
+        $signature = hash_hmac('sha256', $header . '.' . $payload, $secret, true);
         $base64UrlSignature = UrlEncode::base64UrlEncode($signature);
-        return  $this->header . '.' . $this->payload . '.' . $base64UrlSignature;
+        return  $header . '.' . $payload . '.' . $base64UrlSignature;
     }
 
-    
+    public static function decode($jwt = '.', $listJwt = 'Authorization', $type = 1){
+        $secret = getenv('SECRET_JWT');
+        if($jwt == '.'){
+            $headerApache = apache_request_headers();
+            $beader = explode(' ', $headerApache['Authorization']);
+            $jwt = $beader[$type];
+        }
+        // split the token
+        $tokenParts = explode('.', $jwt);
+        $header = base64_decode($tokenParts[0]);
+        $payload = base64_decode($tokenParts[1]);
+        $signatureProvided = $tokenParts[2];
+
+        // check the expiration time - note this will cause an error if there is no 'exp' claim in the token
+        $expiration = Carbon::createFromTimestamp(json_decode($payload)->exp);
+        self::$tokenExpired = (Carbon::now()->diffInSeconds($expiration, false) < 0);
+
+        // build a signature based on the header and payload using the secret
+        $base64UrlHeader = UrlEncode::base64UrlEncode($header);
+        $base64UrlPayload = UrlEncode::base64UrlEncode($payload);
+        $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $secret, true);
+        $base64UrlSignature = UrlEncode::base64UrlEncode($signature);
+
+        // verify it matches the signature provided in the token
+        self::$signatureValid = ($base64UrlSignature === $signatureProvided);
+
+        return [
+            $header,
+            $payload,
+            $signatureProvided,
+        ];
+
+    }    
 }
