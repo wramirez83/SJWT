18701825b25f374005a8c29c21b1bd0428faad38
Wilson Ramirez Z <wilson.rz@gmail.com>
2022-06-20 13:07:41 -0500

    fix: block try

--- src/SJWT.php
+++ src/SJWT.php
@@ -16,47 +16,55 @@ class SJWT{
     }
 
     public static function encode($payload, $exp = 60){
-        $secret = getenv('SECRET_JWT');
-        $header = StructJWT::setHeader();
-        $payload = StructJWT::setPayload($payload, $exp);
-        $signature = hash_hmac('sha256', $header . '.' . $payload, $secret, true);
-        $base64UrlSignature = UrlEncode::base64UrlEncode($signature);
-        return  $header . '.' . $payload . '.' . $base64UrlSignature;
+        try{
+            $secret = getenv('SECRET_JWT');
+            $header = StructJWT::setHeader();
+            $payload = StructJWT::setPayload($payload, $exp);
+            $signature = hash_hmac('sha256', $header . '.' . $payload, $secret, true);
+            $base64UrlSignature = UrlEncode::base64UrlEncode($signature);
+            return  $header . '.' . $payload . '.' . $base64UrlSignature;
+        }catch(\Exception $e){
+            return $e;
+        }
     }
 
     public static function decode($jwt = '.', $listJwt = 'Authorization', $type = 1){
-        $secret = getenv('SECRET_JWT');
-        if($jwt == '.'){
-            $headerApache = apache_request_headers();
-            $beader = explode(' ', $headerApache['Authorization']);
-            $jwt = $beader[$type];
+        try{
+            $secret = getenv('SECRET_JWT');
+            if($jwt == '.'){
+                $headerApache = apache_request_headers();
+                $beader = explode(' ', $headerApache['Authorization']);
+                $jwt = $beader[$type];
+            }
+            // split the token
+            $tokenParts = explode('.', $jwt);
+            $header = base64_decode($tokenParts[0]);
+            $payload = base64_decode($tokenParts[1]);
+            $signatureProvided = $tokenParts[2];
+    
+            // check the expiration time - note this will cause an error if there is no 'exp' claim in the token
+            $expiration = Carbon::createFromTimestamp(json_decode($payload)->exp);
+            $tokenExpired = (Carbon::now()->diffInSeconds($expiration, false) < 0);
+    
+            // build a signature based on the header and payload using the secret
+            $base64UrlHeader = UrlEncode::base64UrlEncode($header);
+            $base64UrlPayload = UrlEncode::base64UrlEncode($payload);
+            $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $secret, true);
+            $base64UrlSignature = UrlEncode::base64UrlEncode($signature);
+    
+            // verify it matches the signature provided in the token
+            $signatureValid = ($base64UrlSignature === $signatureProvided);
+    
+            return [
+                $header,
+                $payload,
+                $signatureProvided,
+                $signatureValid,
+                $tokenExpired
+            ];
+        }catch(\Exception $e){
+            return $e;
         }
-        // split the token
-        $tokenParts = explode('.', $jwt);
-        $header = base64_decode($tokenParts[0]);
-        $payload = base64_decode($tokenParts[1]);
-        $signatureProvided = $tokenParts[2];
-
-        // check the expiration time - note this will cause an error if there is no 'exp' claim in the token
-        $expiration = Carbon::createFromTimestamp(json_decode($payload)->exp);
-        $tokenExpired = (Carbon::now()->diffInSeconds($expiration, false) < 0);
-
-        // build a signature based on the header and payload using the secret
-        $base64UrlHeader = UrlEncode::base64UrlEncode($header);
-        $base64UrlPayload = UrlEncode::base64UrlEncode($payload);
-        $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $secret, true);
-        $base64UrlSignature = UrlEncode::base64UrlEncode($signature);
-
-        // verify it matches the signature provided in the token
-        $signatureValid = ($base64UrlSignature === $signatureProvided);
-
-        return [
-            $header,
-            $payload,
-            $signatureProvided,
-            $signatureValid,
-            $tokenExpired
-        ];
 
     }
 }
